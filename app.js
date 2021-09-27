a73ab9a6c8308ae7a6d9beae7633316ca356d716
Antoine ROBIN <antooinerobin@gmail.com>
2021-09-27 15:32:47 +0200

    Ajout layout et pages

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const port = 3000
 
 app.use('/assets', express.static('assets'))
 
 // set the view engine to ejs
+app.use(expressLayouts)
+app.set('layout', './layouts/layout')
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('pages/index')
+})
+
+app.get('/services', (req, res) => {
+    res.render('pages/services')
+})
+
+app.get('/updates', (req, res) => {
+    res.render('pages/updates')
 })
 
 app.listen(port, () => {
